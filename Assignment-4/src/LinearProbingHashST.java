bb09e796b854603f21126ed2d8c1047de3dc879c
25BurnettA <152207480+25BurnettA@users.noreply.github.com>
2024-12-02 11:25:09 -0600

    Create LinearProbingHashST.java

---
+++ Assignment-4/src/LinearProbingHashST.java
@@ -0,0 +1,38 @@
+public class LinearProbingHashST<Key, Value>
+{
+    private int M = 20000;
+    private Value[] vals = (Value[]) new Object[M];
+    private Key[] keys = (Key[]) new Object[M];
+
+    private int hash(Key key)
+    {
+        System.out.println("here");
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    private void put(Key key, Value val)
+    {
+        int i;
+        for (i = hash(key); keys[i] != null; i = (i+1) % M)
+        {
+            if (keys[i].equals(key))
+            {
+                break;
+            }
+        }
+        keys[i] = key;
+        vals[i] = val;
+    }
+
+    public Value get(Key key)
+    {
+        for (int i = hash(key); keys[i] != null; i = (i+1) % M)
+        {
+            if (key.equals(keys[i]))
+            {
+                return vals[i];
+            }
+        }
+        return null;
+    }
+}
